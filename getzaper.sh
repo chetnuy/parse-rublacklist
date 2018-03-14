f237cea5a52098e91d2f15ce3161438a9c880fd1
chetnuy <chetnuy@gmail.com>
2018-03-14 14:56:55 +0300

    ver 0.2

--- getzaper.sh
+++ getzaper.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
-#mkdir list
-cd list
-#git clone https://github.com/zapret-info/z-i.git
-cat dump.csv | sed 1d | cut -d ';' -f 3 | tr "\|" "\n" |sed 's/^[ \t]*//;s/[ \t]*$//' |uniq >list.txt 
+# this script download mirror russian bloack list and parse it
+# for squid-proxy project 
 
+wget https://github.com/zapret-info/z-i/raw/master/dump.csv
+cat dump.csv | sed 1d | cut -d ';' -f 3 | tr "\|" "\n" |sed 's/^[ \t]*//;s/[ \t]*$//' | sed '/^$/d' | uniq >list.txt 
+rm -rf dump.csv
+
+cp list.txt /etc/squid/
