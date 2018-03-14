c4608b717b48b0eb42694a63070ad2a1b12193db
chetnuy <chetnuy@gmail.com>
2018-03-14 18:52:14 +0300

    delete space

--- getzaper.sh
+++ getzaper.sh
@@ -4,6 +4,7 @@
 
 wget https://github.com/zapret-info/z-i/raw/master/dump.csv
 cat dump.csv | sed 1d | cut -d ';' -f 3 | tr "\|" "\n" |sed 's/^[ \t]*//;s/[ \t]*$//' | sed '/^$/d' | uniq >list.txt 
-rm -rf dump.csv
 
 cp list.txt /etc/squid/
+rm -rf dump.csv
+rm -rf list.txt
